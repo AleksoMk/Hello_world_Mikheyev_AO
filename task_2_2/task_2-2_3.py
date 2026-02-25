35192f95595b4371d316c9cc6e0dd168e5449240
AleksoMk <sasha.mikheyev.2013@gmail.com>
2026-02-25 16:33:12 +0300

    Delete task_2_2 directory

--- task_2_2/task_2-2_3.py
+++
@@ -1,6 +0,0 @@
-device_name = "flugegeheimen"
-inv_number = 69
-condition = "good"
-quantity = 1
-
-print(f"Название прибора\t\tИнвентарный номер\tСостояние\tКоличество\n{device_name}\t{inv_number}\t\t{condition}\t\t{quantity}")
