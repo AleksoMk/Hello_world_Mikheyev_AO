d2207cfb52e5c5541b87d7fd470cd0aaa1823f71
AleksoMk <sasha.mikheyev.2013@gmail.com>
2026-02-25 16:34:29 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,3 @@
+f = open("output.txt", "w", encoding="utf-8")
+print("Михеев Александр 4731204/50003", file=f)
+f.close()
---
+++ projects_2/task_2_2/task_2-2_1.py
@@ -0,0 +1,5 @@
+course_name = "Информатика"
+current_stage = "2"
+completed_tasks = "6"
+
+print (f"Курс: {course_name}, Текущий этап: {current_stage}, Выполненные задачи : {completed_tasks}")
